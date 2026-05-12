3305f16630745dc8980a8f2855f180b7babd1441
Michalopcd <michal.opa@interia.pl>
2026-05-12 13:51:41 +0200

    poprawnienie LandingPage

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,40 +1,6 @@
-// const LandingPage = () => {
-//   return <div>Landing Page</div>;
-// };
-
-// export default LandingPage;
-
-import { Button} from "../../ui/Button";
-import { Input } from "../../ui/Input";
-import { Card } from "../../ui/Card";
-import { Container } from "../../ui/Container";
-import { Badge } from "../../ui/Badge";
-import { SectionTitle } from "../../ui/SectionTitle";
-import { Checkbox } from "../../ui/Checkbox";
 const LandingPage = () => {
-  return (
-  <Container>
-    <div className="space-y-6 p-6">
-      <SectionTitle>
-        Generator Planów Treningowych
-      </SectionTitle>
-
-      <Badge>
-        Nowy plan
-      </Badge>
-<Checkbox label="Trening aktywny" />
-      <Card>
-        <div className="space-y-4">
-          <Input placeholder="Nazwa treningu" />
-
-          <Button >
-            Zapisz plan
-          </Button>
-        </div>
-      </Card>
-    </div>
-  </Container>
-  )
+  return <div>Landing Page</div>;
 };
 
 export default LandingPage;
+
