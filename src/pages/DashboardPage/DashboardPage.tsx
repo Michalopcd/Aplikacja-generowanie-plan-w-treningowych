d8a0784ec2f79c3101f260f0e22108ee01df943b
Michalopcd <michal.opa@interia.pl>
2026-05-12 21:46:01 +0200

    dodanie layuotu header

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,5 +1,8 @@
+
 const DashboardPage = () => {
   return <div>Dashboard Page</div>;
+  
+  
 };
 
 export default DashboardPage;
---
+++ src/ui/layouts/Header/Header.tsx
@@ -0,0 +1,25 @@
+type HeaderProps={
+    userName?:string,
+    avatarUrl?:string
+}
+
+export  function Header({
+    userName="Jan",
+    avatarUrl,
+}:HeaderProps){
+return(
+ <header className="flex items-center justify-between px-6 py-4">
+    <div>
+        <h1 className="text-lg font-semibold text-white">Cześć {userName}</h1>
+        <p className="text-sm text-muted">Gotowy na kolejny trening?</p>
+    </div>
+<div>
+{avatarUrl ? (
+    <img src={avatarUrl} alt="Avatar użytkownika"   className="h-10 w-10 rounded-full object-cover" />
+):(
+    <div   className="h-10 w-10 rounded-full"/>
+)}
+</div>
+ </header>   
+)
+}
