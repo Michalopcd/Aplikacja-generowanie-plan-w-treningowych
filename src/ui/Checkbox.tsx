8b3786b7a56616c360c8ce73129a5f1f396bb4d8
Michalopcd <michal.opa@interia.pl>
2026-06-06 22:00:22 +0200

    refactor: extend checkbox component props

--- src/ui/Checkbox.tsx
+++ src/ui/Checkbox.tsx
@@ -1,22 +1,27 @@
-type CheckboxProps = {
-  label: string;
-};
+type Props =
+  React.InputHTMLAttributes<HTMLInputElement> & {
+    label: string;
+  };
 
 export function Checkbox({
   label,
-}: CheckboxProps) {
+  className = "",
+  ...props
+}: Props) {
   return (
     <label className="flex items-center gap-2 text-white">
       <input
+        {...props}
         type="checkbox"
-        className="
+        className={`
           h-4
           w-4
           rounded-full
           border-2
           border-zinc-600
           accent-green-500
-        "
+          ${className}
+        `}
       />
 
       {label}
