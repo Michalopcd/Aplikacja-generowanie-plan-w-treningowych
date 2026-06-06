ce0bf551647bd7fdb25e782db46cc758384b5004
Michalopcd <michal.opa@interia.pl>
2026-06-06 21:41:26 +0200

    refactor: extend ui component props

--- src/ui/Card.tsx
+++ src/ui/Card.tsx
@@ -1,10 +1,22 @@
-type CardProps = {
-  children: React.ReactNode;
-};
+type Props = React.HTMLAttributes<HTMLDivElement>;
 
-export function Card({ children }: CardProps) {
+export function Card({
+  children,
+  className = "",
+  ...props
+}: Props) {
   return (
-    <div className="rounded-xl border border-zinc-800 bg-zinc-900 p-4">
+    <div
+      {...props}
+      className={`
+        rounded-xl
+        border
+        border-zinc-800
+        bg-zinc-900
+        p-4
+        ${className}
+      `}
+    >
       {children}
     </div>
   );
