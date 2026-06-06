89bee573d00cff4af503565e98fded7d36cb962e
Michalopcd <michal.opa@interia.pl>
2026-06-06 22:13:30 +0200

    refactor: extend reusable ui component props

--- src/ui/SectionTitle.tsx
+++ src/ui/SectionTitle.tsx
@@ -1,12 +1,21 @@
-type SectionTitleProps = {
-  children: React.ReactNode;
-};
+type SectionTitleProps =
+  React.HTMLAttributes<HTMLHeadingElement>;
 
 export function SectionTitle({
   children,
+  className = "",
+  ...props
 }: SectionTitleProps) {
   return (
-    <h2 className="text-2xl font-bold text-white">
+    <h2
+      {...props}
+      className={`
+        text-2xl
+        font-bold
+        text-white
+        ${className}
+      `}
+    >
       {children}
     </h2>
   );
