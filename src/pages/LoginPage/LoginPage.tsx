d66d8586078da7ccbf2bbc5b76c4ca8fbac2197e
Michalopcd <michal.opa@interia.pl>
2026-06-08 23:26:07 +0200

    feat: create login form with state handling

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+import { Button } from "../../ui/Button";
+import { Input } from "../../ui/Input";
+
 const LoginPage = () => {
-  return <div>Login Page</div>;
-};
+const [email,setEmail]=useState("");
+const [password,setPassword]=useState("");
+
+const handleLogin = (e:React.FormEvent) => {
+  e.preventDefault();
+  console.log(email,password);
+}
+
+return(
+  <form onSubmit={handleLogin}>
+    <Input 
+    type="email"
+    name="email"
+    value={email}
+    onChange={(e)=>setEmail(e.target.value)}
+    />
+    <Input
+    type="password"
+    name="password"
+    value={password}
+    onChange={(e)=>setPassword(e.target.value)}
+    />
+
+    <Button type="submit">Zaloguj się</Button>
+  </form>
+)
+}
 
 export default LoginPage;
