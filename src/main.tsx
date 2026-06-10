9dffa82b6dc15622564076f2141c68ee5d7d16cd
Michalopcd <michal.opa@interia.pl>
2026-06-10 14:40:41 +0200

    feat: provide auth context to app

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,9 @@ import "./index.css";
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <React.StrictMode>
+  <AuthProvider>
     <App />
-  </StrictMode>,
+  </AuthProvider>
+</React.StrictMode>
 )
