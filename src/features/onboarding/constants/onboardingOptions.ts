89dc9ba44fec6daf8770619ad2f526f128abe67f
Michalopcd <michal.opa@interia.pl>
2026-06-21 23:19:59 +0200

    feat: add onboarding preferences options

--- src/features/onboarding/constants/onboardingOptions.ts
+++ src/features/onboarding/constants/onboardingOptions.ts
@@ -1,4 +1,16 @@
-export const experienceLevelOptions = [
+import type {
+  ExperienceLevel,
+  Gender,
+  TrainingGoal,
+  TrainingLocation,
+} from "../types/onboarding";
+
+type Option<T extends string> = {
+  value: T;
+  label: string;
+};
+
+export const experienceLevelOptions: Option<ExperienceLevel>[] = [
   {
     value: "beginner",
     label: "Początkujący",
@@ -13,7 +25,7 @@ export const experienceLevelOptions = [
   },
 ];
 
-export const goalOptions = [
+export const goalOptions: Option<TrainingGoal>[] = [
   {
     value: "reduction",
     label: "Redukcja",
@@ -25,5 +37,48 @@ export const goalOptions = [
   {
     value: "mass",
     label: "Masa",
+  },
+];
+export const genderOptions: Option<Gender>[] = [
+  {
+    value: "female",
+    label: "Kobieta",
+  },
+  {
+    value: "male",
+    label: "Mężczyzna",
+  },
+  {
+    value: "prefer_not_to_say",
+    label: "Wolę nie podawać",
+  },
+];
+
+export const trainingLocationOptions: Option<TrainingLocation>[] = [
+  {
+    value: "home",
+    label: "W domu",
+  },
+  {
+    value: "gym",
+    label: "Siłownia",
+  },
+];
+export const trainingDaysOptions: Option<"2" | "3" | "4" | "5">[] = [
+  {
+    value: "2",
+    label: "2 dni w tygodniu",
+  },
+  {
+    value: "3",
+    label: "3 dni w tygodniu",
+  },
+  {
+    value: "4",
+    label: "4 dni w tygodniu",
+  },
+  {
+    value: "5",
+    label: "5 dni w tygodniu",
   },
 ];
