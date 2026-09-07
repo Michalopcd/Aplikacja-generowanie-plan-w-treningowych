82a8f04005ec141aeb979abe8353d8b2fd671d33
Michał <michal.opa@interia.pl>
2026-09-07 19:04:56 +0200

    refactor: delete useNavigation

--- src/pages/TrainingPlanPage/TrainingPlanPage.tsx
+++ src/pages/TrainingPlanPage/TrainingPlanPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 
 import { useAuth } from "../../features/auth/AuthContext";
